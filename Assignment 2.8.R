bd313813a185d0bde58651d873f0663544f37baa
Marissa0304 <m.e.c.okkerman@student.utwente.nl>
2022-10-04 10:30:11 +0200

    assignment 2.8 finished

--- Assignment 2.8.R
+++ Assignment 2.8.R
@@ -4,8 +4,42 @@ library(tidyverse)
 library(foreign)
 
 #Load the data
-choll <- read.spss("data/voorbeeld7_1.sav", to.data.frame = TRUE,
+chol1 <- read.spss("data/voorbeeld7_1.sav", to.data.frame = TRUE,
                    locale = locale(encoding="ISO-8859-1"))
-head(choll)
+head(chol1)
 
 library(ggplot2)
+library(hrbrthemes)
+library(forcats)
+library(stringr)
+
+#Make a scatterplot + regression line (a)
+chol1_plot <- ggplot(chol1, aes(x=leeftijd, y=chol))+
+  geom_point() +
+  geom_smooth(method="lm",
+              formula=y ~ x,
+              col="red")
+print(chol1_plot)
+
+#Fit a linear model (b)
+fit1 <- lm(chol~leeftijd, data=chol1)
+summary(fit1)
+
+#Fit a linear model (c)
+fit2 <- lm(chol~bmi, data=chol1)
+summary(fit2)
+
+fit3 <- lm(chol~sekse, data=chol1)
+summary(fit3)
+
+fit4 <- lm(chol~alcohol, data=chol1)
+summary(fit4)
+
+#Residuals (d)
+chol1$Residuals <- fit2$residuals
+head(chol1)
+
+#Histogram (d)
+chol1_histogram <- ggplot(chol1, aes(x=bmi)) +
+  geom_histogram()
+print(chol1_histogram)
